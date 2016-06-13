1df5bba5c20d1aa5d5b4b88701765b011ca92956
Philippe <philippebs92@hotmail.com>
2016-06-13 20:16:10 -0300

    Criacao da rede de neuronios, onde os neuronios ficaram para o treinamento e para execucao.

---
+++ src/nucleo/Rede.java
@@ -0,0 +1,16 @@
+package nucleo;
+
+import java.util.List;
+import java.util.Map;
+
+public class Rede {
+
+	private Map<Integer, Map<TipoNeuronio, List<Neuronio>>> redeNeuronios;
+	private Map<Integer, Map<TipoNeuronio, Transferencia>> mapTransferencia;
+	private EstruturaRede estruturaRede;
+	
+	
+	
+	
+	
+}
